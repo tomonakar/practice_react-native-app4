84cc08cbc6ed89be8abb20464512301a62d5e79e
tomonakar <tomohisa.nak@gmail.com>
2018-07-23 01:59:44 +0900

    feat: ReduxPersistでデータを保存

--- 03_Production-Ready/production-ready-react-native/App/config/store.js
+++ 03_Production-Ready/production-ready-react-native/App/config/store.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import reducer from '../reducers';
 import rootSaga from './sagas';
@@ -11,8 +13,19 @@ if (process.env.NODE_ENV === 'development') {
   middleware.push(logger);
 }
 
-const store = createStore(reducer, applyMiddleware(...middleware));
+const persistCofig = {
+  key: 'root',
+  storage,
+  blacklist: ['network'],
+};
+const persistReducer = persistCombineReducers(persistCofig, reducer);
 
-sagaMiddleware.run(rootSaga);
+const configureStore = () => {
+  const store = createStore(persistReducer, applyMiddleware(...middleware));
+  sagaMiddleware.run(rootSaga);
+  const persistor = persistStore(store);
 
-export default store;
+  return { store, persistor };
+};
+
+export default configureStore;
