9c2ce757e877fae22d75b167ca239e3a751c14b8
tomonakar <tomohisa.nak@gmail.com>
2018-07-23 08:34:03 +0900

    feat: エラーを消す対応を追加

--- 03_Production-Ready/production-ready-react-native/App/config/sagas.js
+++ 03_Production-Ready/production-ready-react-native/App/config/sagas.js
@@ -1,4 +1,5 @@
 import { takeEvery, call, put, select } from 'redux-saga/effects';
+import { delay } from 'redux-saga';
 
 import {
   CHANGE_BASE_CURRENCY,
@@ -38,10 +39,20 @@ const fetchLatestConversionRates = function* ({ currency }) {
   }
 };
 
+const clearConversionError = function* () {
+  const DELAY_SECONDS = 4;
+  const error = yield select(state => state.currencies.error);
+  if (error) {
+    yield delay(DELAY_SECONDS * 1000);
+    yield put({ type: CONVERSION_ERROR, error: null });
+  }
+};
+
 const rootSaga = function* () {
   yield takeEvery(GET_INITIAL_CONVERSION, fetchLatestConversionRates);
   yield takeEvery(CHANGE_BASE_CURRENCY, fetchLatestConversionRates);
   yield takeEvery(SWAP_CURRENCY, fetchLatestConversionRates);
+  yield takeEvery(CONVERSION_ERROR, clearConversionError);
 };
 
 export default rootSaga;
